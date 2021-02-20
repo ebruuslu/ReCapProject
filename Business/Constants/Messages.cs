b973fb820ef8172d4d54dee62497647fe52ad565
Ebru Uslu <ebruusluuu@outlook.com>
2021-02-20 19:33:47 +0300

    20  subat

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -16,21 +16,25 @@ namespace Business.Constants
         public static string ColorDeleted = "Renk silindi";
         public static string ColorUpdated = "Renk güncellendi";
         public static string ColorsListed = "Renk listelendi";
+        public static string ColorNameInValid = "Renk ismi geçersiz";
 
         public static string BrandAdded = "Marka eklendi";
         public static string BrandDeleted = "Marka silindi";
         public static string BrandListed = "Marka listelendi";
         public static string BrandUpdated = "Marka güncellendi";
+        public static string BrandNameInValid = "Marka ismi geçersiz";
 
         public static string CustomerAdded = "Müşteri eklendi";
         public static string CustomerDeleted = "Müşteri silindi";
         public static string CustomersListed = "Müşteri listelendi";
         public static string CustomerUpdated = "Müşteri güncellendi";
+        public static string CustomerNameInValid = "Müşteri ismi geçersiz";
 
         public static string UserAdded = "Kullanıcı eklendi";
         public static string UserDeleted = "Kullanıcı silindi";
         public static string UsersListed = "Kullanıcı listelendi";
         public static string UserUpdated = "Kullanıcı güncellendi";
+        public static string UserNameInValid = "Kullanıcı ismi geçersiz";
 
 
         public static string RentalAdded = "Yeni kiralama eklendi";
